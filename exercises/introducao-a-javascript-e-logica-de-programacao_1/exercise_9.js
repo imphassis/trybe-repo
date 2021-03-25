51c1ded5130f5cc8b4ada1d87c40d0c7ee51249b
PH Assis <pieit@live.com>
2021-03-25 15:25:05 -0300

    final exercises commit

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_9.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise_9.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-console */
 /* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
-// Programa 8: 3 par
+// Programa 9: Impar
 function par3(a, b, c) {
   let resultado = '';
-  if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+  if (a % 2 === 1 || b % 2 === 1 || c % 2 === 1) {
     resultado = 'true';
   } else {
     resultado = 'false';
@@ -12,5 +12,5 @@ function par3(a, b, c) {
   console.log(resultado);
   return resultado;
 }
-par3(2, 4, 6);
+par3(1, 4, 6);
 par3(1, 3, 5);
