5f147281e3e3a43ac596c559a1e172abb5f59c86
PH Assis <pieit@live.com>
2021-03-29 15:10:38 -0300

    initial commit

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/exercises_5.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercises_5.js
@@ -4,14 +4,14 @@
 /* eslint-disable prefer-const */
 
 // /* eslint-disable prefer-const */
-let number = 25;
+let number = 101;
 for (let i = 1; i <= number; i += 1) {
-  let zero = ' '.repeat(i);
+  let zero = '-'.repeat(i);
   let zone = '*'.repeat(i * 2 - 1);
   if (i === 3) {
     zero = ' ';
   }
-  let sideZero = ' '.repeat((number - i) / 2);
+  let sideZero = '-'.repeat((number - i) / 2);
   if (i % 2 === 1) {
     if (i === 1) {
       console.log(`${sideZero}${zone}${sideZero}`);
