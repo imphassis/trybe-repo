964418fe53dcbf11f25ff7243da24512f5f19b77
PH Assis <pieit@live.com>
2021-04-05 16:02:23 -0300

    final commit

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/funct_5.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/funct_5.js
@@ -1,30 +1,28 @@
 /* eslint-disable no-console */
 /* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
-let array = [2, 3, 2, 5, 8, 2, 3];
+let array = [2, 3, 2, 5, 8, 2, 3, 1];
 
 function mostRepeated(x) {
-  let temp = 0;
+  let emptyBottle = [];
+  let tempCounter = 0;
   let points = 0;
-  let char = '';
-  let tank = [];
+  let char = 0;
 
   for (let i = 0; i < x.length; i += 1) {
-    tank.push(x[i]);
-    for (let j = 0; j < tank.length; j += 1) {
-      let is = x[i];
-      let js = tank[j];
-      if (is === js) {
-        temp += 1;
-        if (temp > points) {
-          points = temp;
+    emptyBottle.push(x[i]);
+    for (let j = 0; j < emptyBottle.length; j += 1) {
+      if (x[i] === emptyBottle[j]) {
+        tempCounter += 1;
+        if (tempCounter > points) {
+          points = tempCounter;
           char = x[i];
         }
       }
-      temp = 0;
     }
+    tempCounter = 0;
   }
-  console.log(char);
+  return char;
 }
 
-mostRepeated(array);
+console.log(mostRepeated(array));
