d16d4bce11d7b2055a4c6716b8da93f256a4d832
PH Assis <pieit@live.com>
2021-04-28 20:14:19 -0300

    tdd_4 commit

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_3/tdd_1.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/tdd_1.js
@@ -1,10 +1,18 @@
 import assert from 'assert';
+import lodash from 'lodash';
 // escreva a função addOne aqui
 
 const myArray = [31, 57, 12, 5];
 const unchanged = [31, 57, 12, 5];
 const expected = [32, 58, 13, 6];
-const addOne = (array) => {};
+
+const addOne = (array) => {
+  let newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    newArray.push(array[i] + 1);
+  }
+  return newArray;
+};
 const output = addOne(myArray);
 
 assert.strictEqual(typeof addOne, 'function');
