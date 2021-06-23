2914740d6115486475dfcddef179d3d9d89cb92f
PH Assis <pieit@live.com>
2021-06-22 23:47:39 -0300

    adding new files

---
+++ exercises/module_2/bloco_15/15.2/random-jokes/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nome: '',
+      email: '',
+    };
+  }
+
+  handleInput(e) {
+    const { name, value } = e.target;
+    console.log(name, value);
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Teste de inputs</h1>
+        <p>
+          Nome:
+          <input
+            data-testid="input-nome"
+            onChange={(e) => this.handleInput(e)}
+            name="nome"
+            value={this.state.nome}
+          />
+        </p>
+        <p>
+          E-mail:
+          <input
+            data-testid="input-email"
+            onChange={(e) => this.handleInput(e)}
+            name="email"
+            value={this.state.email}
+          />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
