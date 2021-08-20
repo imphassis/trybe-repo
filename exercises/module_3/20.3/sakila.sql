c286b296e67e5af151fc25dd16c7be7eb66b06d6
PH Assis <pieit@live.com>
2021-08-19 22:13:14 -0300

    Finished pre-exercises

--- exercises/module_3/20.3/sakila.sql
+++ exercises/module_3/20.3/sakila.sql
@@ -96,3 +96,66 @@ WHERE rental.customer_id IN (269, 239, 126, 399, 142);
 SELECT  *
 FROM sakila.address
 WHERE address.district IN ("QLD", 'Nagasaki', 'California', 'Attika', 'Mandalay', 'Nantou' , 'Texas');
+-- 1 Mostre o nome sobrenome e e-mail dos clientes com os seguintes sobrenomes: hicks crawford henry boyd
+-- mason morales e kennedy ordenados por nome em ordem alfabética.
+SELECT  customer.first_name AS 'Nome'
+       ,customer.last_name  AS 'Sobrenome'
+       ,customer.email      AS 'E-mail'
+FROM sakila.customer
+WHERE customer.last_name IN ('hicks', 'crawford', 'henry', 'boyd', 'mason', 'morales', 'kennedy')
+ORDER BY customer.first_name ASC;
+-- 2 Mostre o e-mail dos clientes com os address_id 172 173 174 175 e 176 ordenados em ordem alfabética.
+SELECT  customer.email AS 'E-mail'
+       ,customer.address_id 'Address ID'
+FROM sakila.customer
+WHERE customer.address_id IN (172, 173, 174, 175, 176)
+ORDER BY customer.email asc;
+-- 3 Descubra quantos pagamentos foram feitos entre 01/05/2005 e 01/08/2005 . Lembre-se de que
+-- no banco de dados AS datas estão armazenadas no formato ano/mês/dia diferente do
+-- formato brasileiro que é dia/mês/ano .
+SELECT  *
+FROM sakila.payment
+WHERE payment_date BETWEEN '2005-05-010' AND '2005-08-01';
+-- 4 Mostre o título ano de lançamento e duração do empréstimo de todos os filmes com a duração de
+-- empréstimo de 3 a 6 . Os resultados devem ser classificados em filmes com maior duração de
+-- empréstimo primeiro. Em caso de empate ordene em ordem alfabética pelo título.
+SELECT  film.title           AS 'Título'
+       ,film.release_year    AS 'Ano de lançamento'
+       ,film.rental_duration AS 'Duração do empréstimo'
+FROM sakila.film
+WHERE film.rental_duration BETWEEN 3 AND 6
+ORDER BY film.rental_duration DESC , film.title ASC;
+-- 5 Monte um relatório que exiba o título e classificação dos 500 primeiros filmes direcionados para AS
+-- classificações indicativas G PG e PG-13 . Os resultados devem estar ordenados por título.
+SELECT  film.title  AS 'Título'
+       ,film.rating AS 'Classificação'
+FROM sakila.film
+WHERE film.rating IN ('G', 'PG', 'PG-13')
+ORDER BY film.title ASC
+LIMIT 500;
+-- 1 Quantos pagamentos temos com a data de retorno 2005-05-25 ?
+-- Há múltiplas maneiras possíveis de encontrar a resposta.
+SELECT  COUNT(*) AS 'Quantidade de Pagamentos'
+FROM sakila.payment
+WHERE payment.payment_date like '%2005-05-25%';
+-- 2 Quantos pagamentos foram feitos entre 01/07/2005 e 22/08/2005 ?
+SELECT  COUNT(*) AS 'Quantidade de Pagamentos'
+FROM sakila.payment
+WHERE DATE(payment.payment_date) BETWEEN '2005-07-01' AND '2005-08-22';
+-- 3 Usando a tabela rental extraia data ano mês dia hora minuto e segundo dos registros com rental_id = 10330.
+-- Utilize a coluna rental_date para extrair AS informações.
+SELECT  rental.rental_date         AS 'Data'
+       ,YEAR(rental.rental_date)   AS 'Ano'
+       ,MONTH(rental.rental_date)  AS 'Mês'
+       ,DAY(rental.rental_date)    AS 'Dia'
+       ,HOUR(rental.rental_date)   AS 'Hora'
+       ,MINUTE(rental.rental_date) AS 'Minuto'
+       ,SECOND(rental.rental_date) AS 'Segundo'
+FROM sakila.rental
+WHERE rental.rental_id = 10330;
+-- 4 Monte uma query que retorne todos os dados do pagamento feito no dia 28/07/2005 a partir das 22 horas .
+SELECT  *
+FROM sakila.payment
+WHERE DATE(payment.payment_date) > '2005-07-28'
+AND HOUR(payment.payment_date) >= '22'
+ORDER BY payment.payment_date ASC;
